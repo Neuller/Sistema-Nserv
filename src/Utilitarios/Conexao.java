ef0e994f0de47e9140b63990d0ef6db12acbac4f
Neuller <Neuller97@hotmail.com>
2018-08-20 11:42:51 -0300

    Commit origin/master V1.0 @uthor Ncesar;

--- src/Utilitarios/Conexao.java
+++ src/Utilitarios/Conexao.java
@@ -21,8 +21,8 @@ public class Conexao {
     // Banco de Dados Hospedado
     private final String URL = "jdbc:mysql://nservbd.mysql.uhserver.com:3306/nservbd";
     private final String Driver = "org.gjt.mm.mysql.Driver";
-    private final String Usuario = "nserv";
-    private final String Senha = "2525@32Nv";
+    private final String Usuario = "";
+    private final String Senha = "";
     private static Connection Con;
     
     
