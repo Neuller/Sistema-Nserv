098a1d54f053bc107ec5961c2532a8831aeeb51d
Neuller <Neuller97@hotmail.com>
2018-09-21 14:32:53 -0300

    Update 21-09
    @uthor Ncesar;

--- src/Utilitarios/Conexao.java
+++ src/Utilitarios/Conexao.java
@@ -22,8 +22,8 @@ public class Conexao {
 
     private final String URL = "jdbc:mysql://nservbd.mysql.uhserver.com:3306/nservbd";
     private final String Driver = "org.gjt.mm.mysql.Driver";
-    private final String Usuario = "nserv";
-    private final String Senha = "2525@32Nv";
+    private final String Usuario = "";
+    private final String Senha = "";
     private static Connection Con;
 
     
